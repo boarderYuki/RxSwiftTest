409e7176ea776d8855dc31627c2340eee98ec76b
yuki.pro <yuki.kr@gmail.com>
2018-06-14 12:41:29 +0900

    델리게이트 사용

---
+++ RxSwift01/DetailViewController.swift
@@ -0,0 +1,43 @@
+//
+//  DetailViewController.swift
+//  RxSwift01
+//
+//  Created by yuki.pro on 2018. 6. 14..
+//  Copyright © 2018년 yuki. All rights reserved.
+//
+
+import UIKit
+
+protocol CharacterDelegate {
+    func didSelectCharacter (_ name:String)
+}
+
+class DetailViewController: UIViewController {
+    
+    
+    var delegate:CharacterDelegate?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func characterSelected(_ sender: UIButton) {
+        guard let characterName = sender.titleLabel?.text else {return}
+        
+        if let delegateObject = delegate {
+            delegateObject.didSelectCharacter(characterName)
+        }
+        
+    }
+    
+    
+}
--- RxSwift01/ViewController.swift
+++ RxSwift01/ViewController.swift
@@ -8,18 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CharacterDelegate {
+    
+    @IBOutlet weak var greetingsLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    
+    @IBAction func selectCharacter(_ sender: Any) {
+        
+        let detailVC = storyboard?.instantiateViewController(withIdentifier: "DetailView") as! DetailViewController
+        detailVC.delegate = self
+        
+        
+        navigationController?.pushViewController(detailVC, animated: true)
+        
+    }
+    
+    func didSelectCharacter(_ name: String) {
+        self.greetingsLabel.text = "Hello \(name)"
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
