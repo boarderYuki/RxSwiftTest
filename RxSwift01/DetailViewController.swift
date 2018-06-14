4880c8716758da2cc7bb211ca5fb2ace616cefca
yuki.pro <yuki.kr@gmail.com>
2018-06-14 15:49:10 +0900

    no message

--- RxSwift01/DetailViewController.swift
+++ RxSwift01/DetailViewController.swift
@@ -20,14 +20,12 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-        
         // Do any additional setup after loading the view.
     }
     
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     @IBAction func characterSelected(_ sender: UIButton) {
@@ -36,7 +34,6 @@ class DetailViewController: UIViewController {
         if let delegateObject = delegate {
             delegateObject.didSelectCharacter(characterName)
         }
-        
     }
     
     
